d3fd6ba2a0b34eef1582c4250a7e6f27159c8586
cristiangarzon22 <86244196+cristiangarzon22@users.noreply.github.com>
2023-02-05 23:38:13 -0500

    color-switcher-js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+  const inicio = document.querySelector('[data-start=""]');
+  const detener = document.querySelector('[data-stop=""]');
+  let process;
+  const body = document.querySelector("body");
 
+  detener.disabled = true;
+
+  inicio.addEventListener("click" , () => {
+        process = setInterval(() => {
+            body.style.backgroundColor = '#' + (((1 << 24) * Math.random()) | 0).toString(16);
+            inicio.disabled = true;
+            detener.disabled = false;
+        }, 1000);
+  });
+
+  detener.addEventListener("click" , () => {
+    clearInterval(process);
+    inicio.disabled = false;
+    detener.disabled = true;
+  });
+
+  
